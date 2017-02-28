5455d3571e425af03dfe6367a893955867914c1b
ranjithamca <ranjithar@megam.io>
2017-02-28 16:19:01 +0530

    route modify

--- site/riak/riak.lua
+++ site/riak/riak.lua
@@ -3,7 +3,7 @@
 
 -- wget riak package
 arc = resource.shell.new("cd /var/lib")
-arc.command = "wget -O /var/lib/riak-2.1.3-1.el6.x86_64.rpm http://s3.amazonaws.com/downloads.basho.com/riak/2.1/2.1.3/rhel/6/riak-2.1.3-1.el6.x86_64.rpm
+arc.command = "wget -O /var/lib/riak-2.1.3-1.el6.x86_64.rpm http://s3.amazonaws.com/downloads.basho.com/riak/2.1/2.1.3/rhel/6/riak-2.1.3-1.el6.x86_64.rpm"
 
 -- install riak rpm package
 des = resource.shell.new("des")
