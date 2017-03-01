7be0045c9ca6c9d0cff40669e28a4da5f60920ee
ranjithamca <ranjithar@megam.io>
2017-03-01 12:40:37 +0530

    bitnami changes

--- site/bitnami/bitnami.lua
+++ site/bitnami/bitnami.lua
@@ -8,30 +8,16 @@ dir = "/var/lib/megam/"
 f = loadfile(dir .. "gru/gulp/param.lua")
 f()
 
-bitnami_dir = "/var/lib/megam/bitnami/"
+bitnami_dir = "/var/lib/megam/gru/site/bitnami/remote.sh "
 bitnami_file = "/var/lib/megam/bitnami/bitnami-run"
-
--- create bitnami  directory
-
-dir = io.open(bitnami_file, "r")
-
-if dir ~= nil then
-bit_dir = resource.shell.new(bitnami_dir)
- bit_dir.command = "rm -rf" .. bitnami_dir
-end
-
- bit_dir = resource.directory.new(bitnami_dir)
- bit_dir.state = "present"
-
---get a file from remote repository
-
-remote = resource.shell.new("remote")
-remote.state = "present"
-remote.command = "wget -O /var/lib/megam/bitnami/bitnami-run  " .. bitnami_url
 
 --change permission of file
 mode = resource.shell.new("mode")
-mode.command = "chmod 755 " .. bitnami_file
+mode.command = "chmod 755 " .. bitnami_dir
+
+exe = resource.shell.new("sh")
+exe.command = bitnami_dir  ..   bitnami_url
+
 
 src = resource.shell.new("db")
 
@@ -54,4 +40,4 @@ src.command = bitnami_file  .. " --mode unattended  --base_user " .. bitnami_use
 end
 
 -- Finally, register the resources to the catalog
-catalog:add(bit_dir, remote, mode, src)
+catalog:add( mode, exe, src)
