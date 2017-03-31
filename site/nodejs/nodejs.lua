14d938266f4163a653683ec4425aa131da006996
ranjithamca <ranjithar@megam.io>
2017-03-31 17:33:45 +0530

    file added

--- site/nodejs/nodejs.lua
+++ site/nodejs/nodejs.lua
@@ -1,5 +1,5 @@
 --
--- Gru module for installing and configuring couchdb
+-- Gru module for installing and configuring nodejs app
 -- The require command respresents the serially execute the process.
 --
 
--- site/play/play.lua
+++ site/play/play.lua
@@ -1,5 +1,5 @@
 --
--- Gru module for installing and configuring java app
+-- Gru module for installing and configuring play
 -- The require command respresents the serially execute the process.
 --
 
